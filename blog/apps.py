b2b157441f8620e3f2c19c7e221213c07137c83e
Angus Friel <angus.friel.1986@gmail.com>
2024-02-27 14:10:51 +0000

    Add comments

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
+# Define the configuration for the 'blog' app
+
 
 class BlogConfig(AppConfig):
+    # Set the default primary key field for models to BigAutoField
     default_auto_field = 'django.db.models.BigAutoField'
+    # Set the name of the app
     name = 'blog'
