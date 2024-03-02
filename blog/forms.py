efeb22b6aeb403b3dd1a2257054e51c707fab6a2
Angus Friel <angus.friel.1986@gmail.com>
2024-03-02 11:00:47 +0000

    Add docstrings to classes

--- blog/forms.py
+++ blog/forms.py
@@ -2,19 +2,32 @@ from .models import Comment  # Import Comment model
 from .models import Contact  # Import Contact model
 from django import forms  # Import forms module from django
 
-# Define CommentForm class for handling comment form data
-
 
 class CommentForm(forms.ModelForm):
+    """
+    A form for creating and submitting a comment.
+    This form is linked to the
+    Comment model and includes only the
+    'body' field for submission.
+    """
     class Meta:
-        model = Comment  # Specify the model to be used for the form
-        fields = ('body',)  # Specify the fields to be included in the form
-
-# Define ContactForm class for handling contact form data
+        # Specify the model to be used for the form
+        model = Comment
+        # Specify the fields to be included in the form
+        fields = ('body',)
 
 
 class ContactForm(forms.ModelForm):
+    """
+    A form for submitting contact information and messages.
+    This form is linked
+    to the Contact model and includes fields for name,
+    email, subject, and message.
+    It is used to facilitate users in sending
+    messages or inquiries.
+    """
     class Meta:
-        model = Contact  # Specify the model to be used for the form
+        # Specify the model to be used for the form
+        model = Contact
         # Specify the fields to be included in the form
         fields = ['name', 'email', 'subject', 'message']
