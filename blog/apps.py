0acceb9c364dc6af485cbf365f74a349f33859b5
Angus Friel <angus.friel.1986@gmail.com>
2024-03-02 10:59:27 +0000

    Change # comments to docstrings

--- blog/apps.py
+++ blog/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 
-# Define the configuration for the 'blog' app
-
 
 class BlogConfig(AppConfig):
-    # Set the default primary key field for models to BigAutoField
+    """
+    Set the default primary key field for models to BigAutoField
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     # Set the name of the app
     name = 'blog'
