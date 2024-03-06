2b58f12c38065fa072b913511e1fad98f18bec58
Angus Friel <angus.friel.1986@gmail.com>
2024-03-06 10:58:43 +0000

    Still trying to change code to force the 500.html page

--- brutalgroove/views.py
+++ brutalgroove/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponseServerError
 
 
-# def handler404(request, exception):
-#     """ Error Handler 404 - Page Not Found """
-#     return render(request, "errors/404.html", status=404)
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
 
 
 def handler500(request):
     """ Error Handler 500 - Internal Server Error """
-    try:
-        1/0
-        return render(request, "errors/500.html", status=500)
-    except Exception:
-        raise HttpResponseServerError("errors/500.html", status=500)
+    # return render(request, "errors/500.html", status=500)
+    raise Exception(request, "errors/500.html", status=500)
