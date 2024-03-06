258e3e73cf3975ddaf2cd2a4c040fa3d10d809b2
Angus Friel <angus.friel.1986@gmail.com>
2024-03-06 10:37:09 +0000

    Comment out code to force the 500 error

--- brutalgroove/views.py
+++ brutalgroove/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponseServerError
 
 
-def handler404(request, exception):
-    """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+# def handler404(request, exception):
+#     """ Error Handler 404 - Page Not Found """
+#     return render(request, "errors/404.html", status=404)
 
 
 def handler500(request):
