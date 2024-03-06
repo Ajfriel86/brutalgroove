315437eece3d5e40bc3ea3606ce94cd1680e1319
Angus Friel <angus.friel.1986@gmail.com>
2024-03-06 10:34:55 +0000

    Add to view to try trigger 500 error page

--- brutalgroove/views.py
+++ brutalgroove/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseServerError
 
 
 def handler404(request, exception):
@@ -8,4 +9,8 @@ def handler404(request, exception):
 
 def handler500(request):
     """ Error Handler 500 - Internal Server Error """
-    return render(request, "errors/500.html", status=500)
+    try:
+        1/0
+        return render(request, "errors/500.html", status=500)
+    except Exception:
+        raise HttpResponseServerError("Internal Server Error")
